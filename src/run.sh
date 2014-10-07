d2dacd74c6c5890424065cbef3348c2ec7b1e3e8
LiuYan 刘研 <lovetide@qq.com>
2014-10-07 11:22:57 +0800

    更新库文件： jsoup: 1.7.3->1.8.1, geoip2: 0.8.0->2.0.0, maxmind-db: 0.3.0->1.0.0; mysql-connector-java: 5.1.32->5.1.33

--- src/run.sh
+++ src/run.sh
@@ -7,11 +7,11 @@ channels=${channels-LiuYanBot,linuxba,fedora-zh}
 export _JAVA_OPTIONS="-Dbotcmd.prefix=$botcmdPrefix -Dmessage.delay=400 -Djava.util.logging.config.file=$botdir/logging.properties -Djavax.net.ssl.trustStore=$dbdir/GoAgentCA.jks -DGoAgent.proxyHost=${GoAgent_proxyHost-192.168.2.1} -DGoAgent.proxyPort=${GoAgent_proxyPort-8087} -Ddatabase.driver=${database_driver-com.mysql.jdbc.Driver} -Ddatabase.username=${database_username-bot} -Ddatabase.userpassword=${database_userpassword} -Ddatabase.url=${database_url-jdbc:mysql://192.168.2.1/bot?autoReconnect=true&amp;characterEncoding=UTF-8&amp;zeroDateTimeBehavior=convertToNull} $_JAVA_OPTIONS"
 
 for lf in commons-lang3-3.3.2 commons-io-2.4 commons-exec-1.1 commons-logging-1.2 commons-pool2-2.2 commons-dbcp2-2.0.1 commons-codec-1.9 \
-    mysql-connector-java-5.1.32-bin  mariadb-java-client-1.1.7 \
+    mysql-connector-java-5.1.33-bin  mariadb-java-client-1.1.7 \
     jackson-core-2.4.2  jackson-databind-2.4.2  jackson-annotations-2.4.2 \
-    maxmind-db-0.3.3  geoip2-0.8.0  \
+    maxmind-db-1.0.0  geoip2-2.0.0  \
     google-pagerank-api-2.0 \
-    jsoup-1.7.3 \
+    jsoup-1.8.1 \
     QQWry StackExchangeAPI pircbot-mod
 do
 	CP="${CP}$libdir/$lf.jar:"
