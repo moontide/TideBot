e70be30c80c0bc5915ce57747863f160e45fa2bb
LiuYan 刘研 <lovetide@qq.com>
2015-01-07 01:32:57 +0800

    jackson 库升级到 2.5.0; commons-pool2 库升级到 2.3

--- src/run.sh
+++ src/run.sh
@@ -4,11 +4,11 @@ dbdir=$(readlink -e "$botdir/../db")
 nick=${nick-CmdBot}
 channels=${channels-LiuYanBot,linuxba,fedora-zh}
 
-export _JAVA_OPTIONS="-Dbotcmd.prefix=$botcmdPrefix -Dmessage.delay=400 -Djava.util.logging.config.file=$botdir/logging.properties -Djavax.net.ssl.trustStore=$dbdir/GoAgentCA.jks -DGoAgent.proxyHost=${GoAgent_proxyHost-192.168.2.1} -DGoAgent.proxyPort=${GoAgent_proxyPort-8087} -Ddatabase.driver=${database_driver-com.mysql.jdbc.Driver} -Ddatabase.username=${database_username-bot} -Ddatabase.userpassword=${database_userpassword} -Ddatabase.url=${database_url-jdbc:mysql://192.168.2.1/bot?autoReconnect=true&amp;characterEncoding=UTF-8&amp;zeroDateTimeBehavior=convertToNull} $_JAVA_OPTIONS"
+export _JAVA_OPTIONS="-Dbotcmd.prefix=$botcmdPrefix -Dmessage.delay=500 -Djava.util.logging.config.file=$botdir/logging.properties -Djavax.net.ssl.trustStore=$dbdir/GoAgentCA.jks -DGoAgent.proxyHost=${GoAgent_proxyHost-192.168.2.1} -DGoAgent.proxyPort=${GoAgent_proxyPort-8087} -Ddatabase.driver=${database_driver-com.mysql.jdbc.Driver} -Ddatabase.username=${database_username-bot} -Ddatabase.userpassword=${database_userpassword} -Ddatabase.url=${database_url-jdbc:mysql://192.168.2.1/bot?autoReconnect=true&amp;characterEncoding=UTF-8&amp;zeroDateTimeBehavior=convertToNull} $_JAVA_OPTIONS"
 
-for lf in commons-lang3-3.3.2 commons-io-2.4 commons-exec-1.1 commons-logging-1.2 commons-pool2-2.2 commons-dbcp2-2.0.1 commons-codec-1.9 \
+for lf in commons-lang3-3.3.2 commons-io-2.4 commons-exec-1.1 commons-logging-1.2 commons-pool2-2.3 commons-dbcp2-2.0.1 commons-codec-1.9 \
     mysql-connector-java-5.1.34-bin  mariadb-java-client-1.1.7 \
-    jackson-core-2.4.4  jackson-databind-2.4.4  jackson-annotations-2.4.4 \
+    jackson-core-2.5.0  jackson-databind-2.5.0  jackson-annotations-2.5.0 \
     maxmind-db-1.0.0  geoip2-2.1.0  \
     google-pagerank-api-2.0 \
     jsoup-1.8.1 \
