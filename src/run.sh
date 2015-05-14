5ecbedab3338b99b60fcd271a724bf85f9a69cf3
LiuYan 刘研 <lovetide@qq.com>
2015-05-14 18:30:59 +0800

    库升级； 翻墙代理变量名更改； GoAgent 代理 -> shadowsocks 代理
    库升级
    - jackson 2.5.2 -升级到-> 2.5.3
    - geoip2 2.1.0 -升级到-> 2.2.0
    - commons-lang3 3.3.2 -升级到-> 3.4
    
    原先 GoAgent 代理的变量名改为 GFWProxy，并增加了代理类型 GFWProxyType / GFWProxy.Type 变量：取值 http 或 socks 或不设置
    
    原先 GoAgent 代理深圳已经不能使用了，改用 shadowsocks 代理测试

--- src/run.sh
+++ src/run.sh
@@ -5,14 +5,16 @@ nick=${nick-CmdBot}
 channels=${channels-LiuYanBot,linuxba,fedora-zh}
 server=${server-chat.freenode.net}
 
-export _JAVA_OPTIONS="-Dbotcmd.prefix=$botcmdPrefix -Dmessage.delay=1000 -Djava.util.logging.config.file=$botdir/logging.properties -Djavax.net.ssl.trustStore=$dbdir/bot.jks -DGoAgent.proxyHost=${GoAgent_proxyHost-192.168.2.1} -DGoAgent.proxyPort=${GoAgent_proxyPort-8087} -Ddatabase.driver=${database_driver-com.mysql.jdbc.Driver} -Ddatabase.username=${database_username-bot} -Ddatabase.userpassword=${database_userpassword} -Ddatabase.url=${database_url-jdbc:mysql://192.168.2.1/bot?autoReconnect=true&amp;characterEncoding=UTF-8&amp;zeroDateTimeBehavior=convertToNull} $_JAVA_OPTIONS"
+#-Djavax.net.debug=ssl,handshake,record -Ddeployment.security.TLSv1.2=false 
+export _JAVA_OPTIONS="-Dbotcmd.prefix=$botcmdPrefix -Dmessage.delay=1000 -Djava.util.logging.config.file=$botdir/logging.properties -DGFWProxy.TrustStore=$dbdir/bot.jks -DGFWProxy.Type=${GFWProxyType} -DGFWProxy.Host=${GFWProxyHost-192.168.2.1} -DGFWProxy.Port=${GFWProxyPort-8087} -Ddatabase.driver=${database_driver-com.mysql.jdbc.Driver} -Ddatabase.username=${database_username-bot} -Ddatabase.userpassword=${database_userpassword} -Ddatabase.url=${database_url-jdbc:mysql://192.168.2.1/bot?autoReconnect=true&amp;characterEncoding=UTF-8&amp;zeroDateTimeBehavior=convertToNull} $_JAVA_OPTIONS"
 
-for lf in commons-lang3-3.3.2 commons-io-2.4 commons-exec-1.3 commons-logging-1.2 commons-pool2-2.3 commons-dbcp2-2.0.1 commons-codec-1.9 \
+for lf in commons-lang3-3.4 commons-io-2.4 commons-exec-1.3 commons-logging-1.2 commons-pool2-2.3 commons-dbcp2-2.0.1 commons-codec-1.9 \
     mysql-connector-java-5.1.35-bin  mariadb-java-client-1.1.8 \
-    jackson-core-2.5.2  jackson-databind-2.5.2  jackson-annotations-2.5.2 \
-    maxmind-db-1.0.0  geoip2-2.1.0  \
+    jackson-core-2.5.3  jackson-databind-2.5.3  jackson-annotations-2.5.3 \
+    maxmind-db-1.0.0  geoip2-2.2.0  \
     google-pagerank-api-2.0 \
     jsoup-1.8.2 \
+    bsh-2.0b4 \
     QQWry StackExchangeAPI pircbot-mod
 do
 	CP="${CP}$libdir/$lf.jar:"
@@ -29,7 +31,3 @@ java net.maclife.irc.LiuYanBot \
 	-u $nick \
 	-c "$channels" \
 	/ban "$ban" \
-
-#-Djavax.net.ssl.trustStorePassword=changeit \
-
-# 2>&1
--- src/runs.sh
+++ src/runs.sh
@@ -12,7 +12,12 @@ export database_username=bot
 #export database_passwrd=
 export database_url="jdbc:mysql://192.168.115.88/bot?autoReconnect=true&amp;characterEncoding=UTF-8&amp;zeroDateTimeBehavior=convertToNull"
 
-export GoAgent_proxyHost=localhost
-export GoAgent_proxyPort=8087
+#export GFWProxyType=http
+#export GFWProxyHost=localhost
+#export GFWProxyPort=8087
+
+export GFWProxyType=socks
+export GFWProxyHost=192.168.90.201
+export GFWProxyPort=9999
 
 $botdir/run.sh
