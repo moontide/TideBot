573172c9a8aaa4864efe70f62e5acc58e96dfa76
LiuYan 刘研 <lovetide@qq.com>
2016-10-12 02:05:38 +0800

    升级了一些 java 类库

--- src/run.sh
+++ src/run.sh
@@ -34,9 +34,9 @@ nick=${nick:-CmdBot}
 export _JAVA_OPTIONS="-Dbotcmd.prefix=$botcmdPrefix -Dmessage.delay=500 -Djava.util.logging.config.file=$botdir/logging.properties -DGFWProxy.TrustStore=$dbdir/bot.jks -DGFWProxy.Type=${GFWProxyType} -DGFWProxy.Host=${GFWProxyHost-192.168.2.1} -DGFWProxy.Port=${GFWProxyPort-8087} -Ddatabase.driver=${database_driver-com.mysql.jdbc.Driver} -Ddatabase.username=${database_username-bot} -Ddatabase.userpassword=${database_userpassword} -Ddatabase.url=${database_url-jdbc:mysql://192.168.2.1/bot?autoReconnect=true&amp;zeroDateTimeBehavior=convertToNull} $_JAVA_OPTIONS"
 
 for lf in commons-lang3-3.4 commons-io-2.5 commons-exec-1.3 commons-logging-1.2 commons-pool2-2.4.1 commons-dbcp2-2.0.1 commons-codec-1.10 \
-    mysql-connector-java-5.1.39-bin  mariadb-java-client-1.4.6 \
-    jackson-core-2.8.2  jackson-databind-2.8.2  jackson-annotations-2.8.2 \
-    maxmind-db-1.2.1  geoip2-2.7.0  \
+    mysql-connector-java-5.1.40-bin  mariadb-java-client-1.5.2 \
+    jackson-core-2.8.3  jackson-databind-2.8.3  jackson-annotations-2.8.3 \
+    maxmind-db-1.2.1  geoip2-2.8.0  \
     google-pagerank-api-2.0 \
     jsoup-1.9.2 \
     bsh-2.0b4 \
