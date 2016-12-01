b42ff1843c8418c89db7e8f3dc749b4b6d7fd235
LiuYan 刘研 <lovetide@qq.com>
2016-12-01 18:14:56 +0800

    更新了一些类库； 日志增加了更改输出格式的配置，现在更适合自己的习惯： yyyy-MM-dd HH:MM:SS.MMM [] : \n

--- src/run.sh
+++ src/run.sh
@@ -3,6 +3,9 @@ libdir=$(readlink -e "$botdir/../lib")
 dbdir=$(readlink -e "$botdir/../db")
 
 OS=$(uname -s)
+
+shopt -s nocasematch
+
 if [[ $OS == CYGWIN* ]]
 then
 	botdir=$(cygpath -w "$botdir")
@@ -31,14 +34,14 @@ nick=${nick:-CmdBot}
 #password=${password:-liuyan}
 
 #-Djavax.net.debug=ssl,handshake,record -Ddeployment.security.TLSv1.2=false
-export _JAVA_OPTIONS="-Dbotcmd.prefix=$botcmdPrefix -Dmessage.delay=500 -Djava.util.logging.config.file=$botdir/logging.properties -DGFWProxy.TrustStore=$dbdir/bot.jks -DGFWProxy.Type=${GFWProxyType} -DGFWProxy.Host=${GFWProxyHost-192.168.2.1} -DGFWProxy.Port=${GFWProxyPort-8087} -Ddatabase.driver=${database_driver-com.mysql.jdbc.Driver} -Ddatabase.username=${database_username-bot} -Ddatabase.userpassword=${database_userpassword} -Ddatabase.url=${database_url-jdbc:mysql://192.168.2.1/bot?autoReconnect=true&amp;zeroDateTimeBehavior=convertToNull} $_JAVA_OPTIONS"
+export _JAVA_OPTIONS="-Dbotcmd.prefix=$botcmdPrefix -Dmessage.delay=100 -Djava.util.logging.config.file=$botdir/logging.properties -DGFWProxy.TrustStore=$dbdir/bot.jks -DGFWProxy.Type=${GFWProxyType} -DGFWProxy.Host=${GFWProxyHost-192.168.2.1} -DGFWProxy.Port=${GFWProxyPort-8087} -Ddatabase.driver=${database_driver-com.mysql.jdbc.Driver} -Ddatabase.username=${database_username-bot} -Ddatabase.userpassword=${database_userpassword} -Ddatabase.url=${database_url-jdbc:mysql://192.168.2.1/bot?autoReconnect=true&amp;zeroDateTimeBehavior=convertToNull} $_JAVA_OPTIONS"
 
 for lf in commons-lang3-3.4 commons-io-2.5 commons-exec-1.3 commons-logging-1.2 commons-pool2-2.4.1 commons-dbcp2-2.0.1 commons-codec-1.10 \
-    mysql-connector-java-5.1.40-bin  mariadb-java-client-1.5.2 \
-    jackson-core-2.8.3  jackson-databind-2.8.3  jackson-annotations-2.8.3 \
+    mysql-connector-java-5.1.40-bin  mariadb-java-client-1.5.4 \
+    jackson-core-2.8.5  jackson-databind-2.8.5  jackson-annotations-2.8.5 \
     maxmind-db-1.2.1  geoip2-2.8.0  \
     google-pagerank-api-2.0 \
-    jsoup-1.9.2 \
+    jsoup-1.10.1 \
     bsh-2.0b4 \
     hcicloud-5.0.0 \
     QQWry StackExchangeAPI pircbot-mod
