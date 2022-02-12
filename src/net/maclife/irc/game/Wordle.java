1c5956ab1afeab3c48e5cd6596e0493071482988
LiuYan 刘研 <lovetide@qq.com>
2022-02-13 01:27:08 +0800

    修正 Wordle 游戏字符判定 present 不完全正确的问题

--- src/net/maclife/irc/game/Wordle.java
+++ src/net/maclife/irc/game/Wordle.java
@@ -94,18 +94,18 @@ System.out.println ("Answer=[" + answer + "]");
 				Dialog dlg = new Dialog (this,
 						bot, bot.dialogs, (t==MAX_GUESS_TIMES?"请随便猜一个 " + sWordToGuess.length () + " 位字母的英文单词":"继续猜…"), false, participants,
 						channel, nick, login, host, botcmd, botCmdAlias, mapGlobalOptions, listCmdEnv, params);
-				Map<String, Object> participantAnswers = bot.executor.submit (dlg).get ();
+				Map<String, Object> participantCandidateAnswers = bot.executor.submit (dlg).get ();
 
 				int nNoAnswerCount = 0;
-				String answer = null;
+				String sCandidateAnswer = null;
 				StringBuilder sb = new StringBuilder ();
 				nCorrect = nPresent = 0;
 				String sDeltaInfo = null;
 				for (int i=0; i<participants.size (); i++)
 				{
 					String p = (String)participants.get (i);
-					answer = (String)participantAnswers.get (p);
-					if (isQuitGameAnswer(answer))
+					sCandidateAnswer = (String)participantCandidateAnswers.get (p);
+					if (isQuitGameAnswer(sCandidateAnswer))
 					{
 						isParticipantWannaQuit = true;
 						break;
@@ -114,7 +114,7 @@ System.out.println ("Answer=[" + answer + "]");
 					if (i != 0)
 						sb.append (",  ");
 
-					if (answer == null)
+					if (sCandidateAnswer == null)
 					{	// 没回答
 						sb.append (p);
 						sb.append (": ");
@@ -132,40 +132,45 @@ System.out.println ("Answer=[" + answer + "]");
 					nNoAnswerCount = 0;
 
 					// 判断并显示结果
-					char[] arrayCharacterResultFlag = new char [sWordToGuess.length()];
+					char[] arrayCharacterOfCandidateAnswerFlags = new char [sWordToGuess.length()];	// c:位置和字符都正确，p:字符存在，但位置不正确，0x00:不存在
+					char[] arraySourceCharacterFlags = new char [sWordToGuess.length()];	// 仅仅用于标记正确答案单词中被用来标记为 p 的字符，以在下一次判断中排除掉该字符
 					String sWordToGuess_LowerCase = sWordToGuess.toLowerCase ();
-					String sAnswer_LowerCase = answer.toLowerCase ();
+					String sCandidateAnswer_LowerCase = sCandidateAnswer.toLowerCase ();
+
+					nCorrect = MarkCorrect (sWordToGuess_LowerCase, sCandidateAnswer_LowerCase, arraySourceCharacterFlags, arrayCharacterOfCandidateAnswerFlags);
+					nPresent = MarkPresent (sWordToGuess_LowerCase, sCandidateAnswer_LowerCase, arraySourceCharacterFlags, arrayCharacterOfCandidateAnswerFlags);
 					for (int j=0; j<sWordToGuess.length(); j++)
 					{
-						sb.append (Colors.WHITE);	//
-						sb.append (',');
-						if (sWordToGuess_LowerCase.charAt (j) == sAnswer_LowerCase.charAt (j))
+						switch (arrayCharacterOfCandidateAnswerFlags[j])
 						{	//
-							arrayCharacterResultFlag[j] = 'c';
-							sb.append (Colors.GREEN.substring (1));	//
-							nCorrect++;
+							case 'c':
+								sb.append (Colors.GREEN);	//
+								sb.append (sCandidateAnswer.charAt (j));
+								sb.append (Colors.NORMAL);
+								break;
+
+							case 'p':
+								sb.append (Colors.YELLOW);	//
+								sb.append (sCandidateAnswer.charAt (j));
+								sb.append (Colors.NORMAL);
+								break;
+
+							default:
+								sb.append (Colors.DARK_GRAY);	//
+								sb.append (sCandidateAnswer.charAt (j));
+								//sb.append ("\u0336");	// \u0336 (utf8: cc b6)长删除线 combining character，用删除线来字符表示不存在
+								sb.append (Colors.NORMAL);
+								break;
 						}
-						else if (IsCharacterPresent(sWordToGuess_LowerCase, sAnswer_LowerCase, j, arrayCharacterResultFlag))
-						{
-							arrayCharacterResultFlag[j] = 'p';
-							sb.append (Colors.OLIVE.substring (1));	//
-							nPresent++;
-						}
-						else
-						{
-							sb.append (Colors.DARK_GRAY.substring (1));	//
-						}
-						sb.append (answer.charAt (j));
-						sb.append (Colors.NORMAL);
 					}
 				}
 
 				if (t==1 || nCorrect==sWordToGuess.length () || isParticipantWannaQuit)
 				{
 					if (isParticipantWannaQuit)
-						bot.SendMessage (channel, "", LiuYanBot.OPT_DO_NOT_OUTPUT_USER_NAME, 1, 游戏信息 ("结束: 有人" + answer));
+						bot.SendMessage (channel, "", LiuYanBot.OPT_DO_NOT_OUTPUT_USER_NAME, 1, 游戏信息 ("结束: 有人" + sCandidateAnswer + ". 正确答案为: " + sWordToGuess));
 					else
-						bot.SendMessage (channel, "", LiuYanBot.OPT_DO_NOT_OUTPUT_USER_NAME, 1, 游戏信息 ("结束: " + (nCorrect==sWordToGuess.length () ? Colors.GREEN + sb + Colors.NORMAL : sb) + ". 答案: " + sWordToGuess));
+						bot.SendMessage (channel, "", LiuYanBot.OPT_DO_NOT_OUTPUT_USER_NAME, 1, 游戏信息 ("结束: " + (nCorrect==sWordToGuess.length () ? Colors.GREEN + sb + Colors.NORMAL : sb) + ". 正确答案: " + sWordToGuess));
 
 					break;
 				}
@@ -192,15 +197,68 @@ System.out.println ("Answer=[" + answer + "]");
 		}
 	}
 
-	boolean IsCharacterPresent (String sWordToGuess_LowerCase, String sAnswer_LowerCase, int iChar, char[] arrayCharacterResultFlag)
+	/**
+	 * 标记字符和位置都正确的情况
+	 * @param sWordToGuess_LowerCase
+	 * @param sCandidateAnswer_LowerCase
+	 * @param arraySourceCharacterFlags
+	 * @param arrayCandidateAnswerCharacterFlags
+	 * @return 正确字符数量
+	 */
+	int MarkCorrect (String sWordToGuess_LowerCase, String sCandidateAnswer_LowerCase, char[] arraySourceCharacterFlags, char[] arrayCandidateAnswerCharacterFlags)
+	{
+		int nCorrect = 0;
+		for (int j=0; j<sWordToGuess_LowerCase.length(); j++)
+		{
+			if (sWordToGuess_LowerCase.charAt (j) == sCandidateAnswer_LowerCase.charAt (j))
+			{
+				arraySourceCharacterFlags[j] = arrayCandidateAnswerCharacterFlags[j] = 'c';
+				nCorrect ++;
+			}
+		}
+		return nCorrect;
+	}
+
+	/**
+	 * 标记字符存在但位置错误的字符。注意：此函数必须在 MarkCorrect 之后执行。
+	 * @param sWordToGuess_LowerCase
+	 * @param sCandidateAnswer_LowerCase
+	 * @param arraySourceCharacterFlags
+	 * @param arrayCandidateAnswerCharacterFlags
+	 * @return 位置错误的字符数量
+	 */
+	int MarkPresent (String sWordToGuess_LowerCase, String sCandidateAnswer_LowerCase, char[] arraySourceCharacterFlags, char[] arrayCandidateAnswerCharacterFlags)
+	{
+		int nPresent = 0;
+		for (int i=0; i<sWordToGuess_LowerCase.length(); i++)
+		{
+			if (arraySourceCharacterFlags[i]!=0x00 || arrayCandidateAnswerCharacterFlags[i]!=0x00)
+				continue;
+			if (IsCharacterPresent(sWordToGuess_LowerCase, sCandidateAnswer_LowerCase, i, arraySourceCharacterFlags, arrayCandidateAnswerCharacterFlags))
+			{
+				arrayCandidateAnswerCharacterFlags[i] = 'p';
+				nPresent ++;
+			}
+		}
+		return nPresent;
+	}
+
+	boolean IsCharacterPresent (String sWordToGuess_LowerCase, String sCandidateAnswer_LowerCase, int iCandidateAnswerCharacter, char[] arraySourceCharacterFlags, char[] arrayCandidateAnswerCharacterFlags)
 	{
 		//return sWordToGuess_LowerCase.indexOf (sAnswer_LowerCase.charAt (iChar)) >= 0;
 		boolean bPresent = false;
-		//for (int i=0; i<sWordToGuess_LowerCase.length (); i++)
-		//{
-		//	if
-		//}
-		bPresent = sWordToGuess_LowerCase.substring (iChar).indexOf (sAnswer_LowerCase.charAt (iChar)) >= 0;
+		for (int i=0; i<sWordToGuess_LowerCase.length (); i++)
+		{
+			if (arraySourceCharacterFlags[i]!=0x00 || i==iCandidateAnswerCharacter || arrayCandidateAnswerCharacterFlags[i]!=0x00)
+				continue;
+			if (sWordToGuess_LowerCase.charAt (i) == sCandidateAnswer_LowerCase.charAt (iCandidateAnswerCharacter))
+			{
+				arraySourceCharacterFlags[i]='p';	// 将正确答案中被标记为字符存在的位置记录下来，下次再判断时，要排除掉该位置
+				bPresent = true;
+				break;
+			}
+		}
+		//bPresent = sWordToGuess_LowerCase.substring (iChar).indexOf (sAnswer_LowerCase.charAt (iChar)) >= 0;
 		return bPresent;
 	}
 }
